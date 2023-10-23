2798a2cc22f202aad62c1dc3a9ea8f202a2da178
Deuses123 <deusdeqds@gmail.com>
2023-10-23 17:05:30 +0600

    add code

---
+++ Practice.Work7/Methods.cs
@@ -0,0 +1,33 @@
+﻿namespace Practice.Work7;
+
+public class Methods
+{
+    private string Template1 { get; set; }
+    private string Template2 { get; set; }
+
+    public Methods(string template1, string template2)
+    {
+        Template1 = template1;
+        Template2 = template2;
+    }
+
+    public static bool operator ==(Methods? o1, Methods? o2)
+    {
+        if (ReferenceEquals(o1, o2)) return true;
+        if (o1 is null || o2 is null) return false;
+        return o1.Template1 == o2.Template1 && o1.Template2 == o2.Template2;
+    }
+    
+    public static bool operator !=(Methods obj1, Methods obj2)
+    {
+        return !(obj1 == obj2);
+    }
+    
+    
+    
+    public override bool Equals(object? obj)
+    {
+        if (obj is Methods a) return this == a;
+        return false;
+    }
+}
---
+++ Practice.Work7/Program.cs
@@ -0,0 +1,15 @@
+﻿using Practice.Work7;
+
+
+
+var a = new Methods("a", "b");
+var b = new Methods("a", "b");
+Console.WriteLine(b==a);
+Console.WriteLine(a.Equals(b) + "\n");
+
+
+var c = new Methods("c", "d");
+var d = new Methods("d", "d");
+
+Console.WriteLine(c!=d);
+Console.WriteLine(!c.Equals(d));
